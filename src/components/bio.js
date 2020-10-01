bb944baf70446d6a9390c824c8e828e8cf280e0d
superlowburn <superlowburn@gmail.com>
2020-09-30 22:16:10 -0300

    Update bio.js

--- src/components/bio.js
+++ src/components/bio.js
@@ -53,7 +53,7 @@ const Bio = () => {
       )}
       {author?.name && (
         <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
+          </strong> {author?.summary || null}
           {` `}
           <a href={`https://twitter.com/${social?.twitter || ``}`}>
             @comforteagle
